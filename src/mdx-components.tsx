42f4ec9f9f214156c7d1b16ad9ac2ef027fe38ae
Shinra <164582571+xivshinra@users.noreply.github.com>
2026-01-14 22:54:41 +0100

    vercel mdx issue

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,11 +1,8 @@
-import type { MDXComponents } from "mdx/types";
 import { PostCallout } from "@/components/posts/PostCallout";
 import { PostSection } from "@/components/posts/PostSection";
 import { PostParagraph } from "@/components/posts/PostParagraph";
 
-export function useMDXComponents(
-  components: MDXComponents = {}
-): MDXComponents {
+export function useMDXComponents(components = {}) {
   return {
     PostCallout,
     PostSection,
