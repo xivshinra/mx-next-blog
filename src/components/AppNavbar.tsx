89a56b2963b697ef625bb2f5ae9f4262ef1a0602
Shinra <164582571+xivshinra@users.noreply.github.com>
2026-01-14 23:03:05 +0100

    add ModeToggle

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -3,12 +3,16 @@
 import { Newspaper } from "lucide-react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { ModeToggle } from "@/components/ModeToggle";
 
 // COMPONENT
 export default function AppNavbar() {
   return (
     <nav className="w-full bg-sidebar p-4 lg:max-w-360 lg:mx-auto flex justify-between items-center sticky z-50 top-0 shadow-md">
-      <Link href="/" className="flex gap-2 items-center">
+      <Link
+        href="/"
+        className="flex gap-2 items-center"
+      >
         <Newspaper size={20} />
         <h1 className="text-xl font-bold">MDX Blog</h1>
       </Link>
@@ -17,8 +21,14 @@ export default function AppNavbar() {
           <Link href="/blog">Blog</Link>
         </li>
         <li>
+          <ModeToggle />
+        </li>
+        <li>
           <Avatar>
-            <AvatarImage src="/assets/placeholder.jpg" alt="Avatar" />
+            <AvatarImage
+              src="/assets/placeholder.jpg"
+              alt="Avatar"
+            />
             <AvatarFallback>SH</AvatarFallback>
           </Avatar>
         </li>
